71e76b030d802cd4729080392910a8a2a1fbe80f
FND <fnd@oda.(none)>
2009-08-25 16:45:38 +0100

    packaging client-side plugins with the distribution
    These components should not have to be downloaded when creating or
    updating an instance.

--- tiddlywebwiki/config.py
+++ tiddlywebwiki/config.py
@@ -4,30 +4,38 @@ Configuration additions for tiddlywebwiki.
 base_tiddlywiki -- the file location of the tiddlywiki
 file into which Tiddlers are pushed when creating
 outgoing TiddlyWiki representations from TiddlyWeb. This
-can be an absolute path or relative to the startup 
+can be an absolute path or relative to the startup
 directory of the server.
 """
 
+import os
+
+
 try:
     from pkg_resources import resource_filename
-    BASE_TIDDLYWIKI = resource_filename('tiddlywebwiki', 'empty.html')
+    RESOURCES_DIRECTORY = resource_filename('tiddlywebwiki', 'empty.html') # XXX: hacky!?
+    RESOURCES_DIRECTORY = os.path.dirname(RESOURCES_DIRECTORY)
 except ImportError:
-    BASE_TIDDLYWIKI = 'tiddlywebwiki/empty.html'
+    RESOURCES_DIRECTORY = 'tiddlywebwiki'
 
+BASE_TIDDLYWIKI = os.path.join(RESOURCES_DIRECTORY, 'empty.html')
+
+CLIENT_PLUGINS = [
+    'TiddlyWebAdaptor.js',
+    'ServerSideSavingPlugin.js',
+    'TiddlyWebConfig.js']
+CLIENT_PLUGINS = [os.path.join(RESOURCES_DIRECTORY, plugin)
+    for plugin in CLIENT_PLUGINS]
 
 config = {
-        'base_tiddlywiki': BASE_TIDDLYWIKI, 
+        'base_tiddlywiki': BASE_TIDDLYWIKI,
         'twanager_plugins': [
             'tiddlywebwiki.fromsvn',
             'tiddlywebwiki.instancer',
             'tiddlywebwiki.twanager'
             ],
         'instance_tiddlers': [
-            ('system', [
-                'http://svn.tiddlywiki.org/Trunk/association/adaptors/TiddlyWebAdaptor.js',
-                'http://svn.tiddlywiki.org/Trunk/association/plugins/ServerSideSavingPlugin.js',
-                'http://svn.tiddlywiki.org/Trunk/association/plugins/TiddlyWebConfig.js'
-                ]),
+            ('system', CLIENT_PLUGINS),
             ],
         'extension_types': {
             'wiki': 'text/x-tiddlywiki',
@@ -39,5 +47,5 @@ config = {
         # XXX the following is, in most cases, redundant
         'wikitext.type_render_map': {
             'text/x-tiddlywiki': 'wikklytextrender',
-            },
+            }
         }
