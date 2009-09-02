1624b4922411d9f110c272d59c0d394cb6e73148
Chris Dent <cdent@oxide.local>
2009-09-02 13:44:01 +0100

    Correct a cyclic dependency problem when install tiddlywebwiki by moving import statements in __init__ into init().
    This should make it possible to install tiddlywebwiki again.

--- tiddlywebwiki/__init__.py
+++ tiddlywebwiki/__init__.py
@@ -2,17 +2,17 @@
 A TiddlyWeb plugin providing a multi-user TiddlyWiki environment.
 """
 
-import tiddlywebwiki.fromsvn
-import tiddlywebwiki.instancer
-import tiddlywebwiki.twanager
-from tiddlyweb.config import merge_config
-from tiddlywebwiki.config import config as twwconfig
-
-
-__version__ = '0.8'
+__version__ = '0.9'
 
 
 def init(config):
+    # These imports must be in init otherwise getting
+    # the version number causes cyclic imports to happen.
+    import tiddlywebwiki.fromsvn
+    import tiddlywebwiki.instancer
+    import tiddlywebwiki.twanager
+    from tiddlyweb.config import merge_config
+    from tiddlywebwiki.config import config as twwconfig
     merge_config(config, twwconfig)
     tiddlywebwiki.fromsvn.init(config)
     tiddlywebwiki.instancer.init(config)
