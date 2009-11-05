5028fc78f8e2fec058faa477ea051498330a76c3
FND <fnd@oda.(none)>
2009-11-05 14:44:53 +0000

    refactored handle_tiddler_div to provide reusable get_tiddler_from_div

--- tiddlywebwiki/tiddlywiki.py
+++ tiddlywebwiki/tiddlywiki.py
@@ -66,31 +66,39 @@ def handle_tiddler_div(bagname, tiddler_div, store):
     """
     Create a new Tiddler from a tiddler div, in BeautifulSoup form.
     """
-    new_tiddler = Tiddler(tiddler_div['title'], bag=bagname)
-
+    tiddler = get_tiddler_from_div(tiddler_div)
+    tiddler.bag = bagname
     try:
-        new_tiddler.text = _html_decode(tiddler_div.find('pre').contents[0])
-    except IndexError:
-        # there are no contents in the tiddler
-        new_tiddler.text = ''
-
-    for attr, value in tiddler_div.attrs:
-        data = tiddler_div.get(attr, None)
-        if data and attr != 'tags':
-            if attr in (['modifier', 'created', 'modified']):
-                new_tiddler.__setattr__(attr, data)
-            elif (attr not in ['title', 'changecount'] and
-                not attr.startswith('server.')):
-                new_tiddler.fields[attr] = data
-    new_tiddler.tags = _tag_string_to_list(tiddler_div.get('tags', ''))
-
-    try:
-        store.put(new_tiddler)
+        store.put(tiddler)
     except OSError, exc:
         # This tiddler has a name that we can't yet write to the
         # store. For now we just state the error and carry on.
         import sys
-        print >> sys.stderr, 'Unable to write %s: %s' % (new_tiddler.title, exc)
+        print >> sys.stderr, 'Unable to write %s: %s' % (tiddler.title, exc)
+
+
+def get_tiddler_from_div(node):
+    """
+    Create a Tiddler from a BeautifulSoup DIV node
+    """
+    tiddler = Tiddler(node['title'])
+    try:
+        tiddler.text = _html_decode(node.find('pre').contents[0])
+    except IndexError:
+        # there are no contents in the tiddler
+        tiddler.text = ''
+
+    for attr, value in node.attrs:
+        data = node.get(attr, None)
+        if data and attr != 'tags':
+            if attr in (['modifier', 'created', 'modified']):
+                tiddler.__setattr__(attr, data)
+            elif (attr not in ['title', 'changecount'] and
+                not attr.startswith('server.')):
+                tiddler.fields[attr] = data
+    tiddler.tags = _tag_string_to_list(node.get('tags', ''))
+
+    return tiddler
 
 
 def _tag_string_to_list(string):
