9cbdb11444e63c53314307679a1d4c23830cbd98
Chris Dent <cdent@oxide.local>
2009-12-01 13:55:29 +0000

    Add the tiddlywebplugins.atom plugin to the default included stuff in tiddlywebwiki.
    This does not surface in the UI in a way of a recent changes feed
    it simply adds the atom serialization.

--- tiddlywebwiki/__init__.py
+++ tiddlywebwiki/__init__.py
@@ -9,8 +9,10 @@ def init(config):
     # These imports must be in init otherwise getting
     # the version number causes cyclic imports to happen.
     import tiddlywebwiki.manage
+    import tiddlywebplugins.atom
     from tiddlyweb.config import merge_config
     from tiddlywebwiki.config import config as twwconfig
     merge_config(config, twwconfig)
     tiddlywebwiki.manage.init(config)
+    tiddlywebplugins.atom.init(config)
     # XXX and add selector for POST a wiki
