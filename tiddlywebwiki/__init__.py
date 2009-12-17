61032c2f71024cbdaa069049c5b52422ce59c15c
FND <fnd@oda.(none)>
2009-12-17 19:53:44 +0000

    automatically initializing plugins

--- tiddlywebwiki/__init__.py
+++ tiddlywebwiki/__init__.py
@@ -2,17 +2,21 @@
 A TiddlyWeb plugin providing a multi-user TiddlyWiki environment.
 """
 
-__version__ = '0.20.0'
+__version__ = '0.20.1'
 
 
 def init(config):
     # These imports must be in init otherwise getting
     # the version number causes cyclic imports to happen.
     import tiddlywebwiki.manage
+    import tiddlywebplugins.status
     import tiddlywebplugins.atom
+    import differ
     from tiddlyweb.config import merge_config
     from tiddlywebwiki.config import config as twwconfig
 
     merge_config(config, twwconfig)
     tiddlywebwiki.manage.init(config)
+    tiddlywebplugins.status.init(config)
     tiddlywebplugins.atom.init(config)
+    differ.init(config)
