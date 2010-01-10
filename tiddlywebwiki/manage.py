b53b43d2eeb995643340558673957b7ce9f26fe4
FND <fnd@oda.(none)>
2010-01-10 12:10:40 +0000

    refactored tiddlywebwiki.manage to avoid global config object
    This was achieved by moving command functions into init scope, which
    seems a little less horrible.
    
    Perhaps twanager commands could be passed an explicit config argument?

--- tiddlywebwiki/manage.py
+++ tiddlywebwiki/manage.py
@@ -1,5 +1,5 @@
 """
-twanager commands made available by tiddlywebwiki.
+TiddlyWebWiki-specific twanager commands
 """
 
 from tiddlyweb.store import Store
@@ -8,47 +8,39 @@ from tiddlyweb.manage import make_command, usage
 from tiddlywebwiki.tiddlywiki import import_wiki_file
 from tiddlywebwiki.importer import import_list
 
-config = None
 
+def init(config):
 
-def init(config_in):
-    global config
-    config = config_in
+    @make_command()
+    def update(args):
+        """Update all instance_tiddlers in the current instance."""
+        from tiddlywebplugins.instancer import Instance
+        instance = Instance('.', config)
+        instance.update_store()
 
+    @make_command()
+    def twimport(args):
+        """Import one or more plugins, tiddlers or recipes in Cook format: <bag> <URI>"""
+        bag = args[0]
+        urls = args[1:]
+        if not bag or not urls:
+            raise IndexError('missing args')
+        import_list(bag, urls, config)
 
-@make_command()
-def update(args):
-    """Update all instance_tiddlers in the current instance."""
-    from tiddlywebplugins.instancer import Instance
-    instance = Instance('.', config)
-    instance.update_store()
+    @make_command()
+    def imwiki(args):
+        """Import tiddlers from a Tiddlywiki document into a bag: <bag> <filename>"""
+        store = _store()
 
+        try:
+            bag_name, filename = args[0:2]
+            import_wiki_file(store, filename, bag_name)
+        except IndexError, exc:
+            usage("index error: %s" % exc)
+        except ValueError, exc:
+            usage("value error: %s" % exc)
 
-@make_command()
-def twimport(args):
-    """Import one or more plugins, tiddlers or recipes in Cook format: <bag> <URI>"""
-    bag = args[0]
-    urls = args[1:]
-    if not bag or not urls:
-        raise IndexError('missing args')
-    import_list(bag, urls, config)
-
-
-@make_command()
-def imwiki(args):
-    """Import tiddlers from a Tiddlywiki document into a bag: <bag> <filename>"""
-    store = _store()
-
-    try:
-        bag_name, filename = args[0:2]
-        import_wiki_file(store, filename, bag_name)
-    except IndexError, exc:
-        usage("index error: %s" % exc)
-    except ValueError, exc:
-        usage("value error: %s" % exc)
-
-
-def _store():
-    """Get our Store from config."""
-    return Store(config['server_store'][0],
+    def _store():
+        """Get our Store from config."""
+        return Store(config['server_store'][0],
             environ={'tiddlyweb.config': config})
