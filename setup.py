b691261d5b650c5465d8b04b89ad552ee174d9da
Chris Dent <cdent@oxide.local>
2010-01-18 16:19:32 +0000

    return beautiful soup and html5lib to the deps.
    The serialization uses them to do processing of html when making
    <title>'s for the produced wikis.

--- setup.py
+++ setup.py
@@ -21,6 +21,8 @@ setup(
         'tiddlywebplugins.twimport',
         'tiddlywebplugins.utils',
         'tiddlywebplugins.instancer>=0.5.5',
+        'html5lib',
+        'BeautifulSoup',
         'wikklytext'],
     include_package_data = True,
     zip_safe = False
